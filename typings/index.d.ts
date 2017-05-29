cd96ce1831f43a57a4a99345519f2a53a72721b3
xxxKOTxxx <kotboriska@gmail.com>
2017-05-29 13:45:25 +0300

    Typings & gitignore

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/index/index.d.ts" />
+/// <reference path="modules/angular-material/index.d.ts" />
+/// <reference path="modules/angular-mocks/index.d.ts" />
+/// <reference path="modules/angular-resource/index.d.ts" />
+/// <reference path="modules/angular-route/index.d.ts" />
+/// <reference path="modules/angular/index.d.ts" />
